529c23d96c8100acc8bdac76e54a7085b3698bb9
apcjlquesada <jlquesada@apc.edu.ph>
2024-05-20 11:56:38 +0000

    edit from codespaces

--- laravel-docker/resources/views/welcome.blade.php
+++ laravel-docker/resources/views/welcome.blade.php
@@ -93,6 +93,7 @@
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Github Browser edit</h2>
                                             <h2 class="text-xl font-semibold text-black dark:text-white">WSL2 Windows  edit</h2>
+                                            <h2 class="text-xl font-semibold text-black dark:text-white">Codespaces  edit</h2>
 
                                             <p class="mt-4 text-sm/relaxed">
                                                 Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.

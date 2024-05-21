d0a1659c6df8274a3cd51c313886ef7c3b068728
Joe Gene Quesada <jlquesada@apc.edu.ph>
2024-05-21 16:41:39 +0800

    update from Windows WSL2

--- laravel-docker/resources/views/welcome.blade.php
+++ laravel-docker/resources/views/welcome.blade.php
@@ -92,7 +92,7 @@
                                         <div class="pt-3 sm:pt-5 lg:pt-0">
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Github Browser edit</h2>
-                                            <h2 class="text-xl font-semibold text-black dark:text-white">WSL2 Windows  edit</h2>
+                                            <h2 class="text-xl font-semibold text-black dark:text-white">WSL2 Windows  edit 2</h2>
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Codespaces  edit 2</h2>
 
                                             <p class="mt-4 text-sm/relaxed">
